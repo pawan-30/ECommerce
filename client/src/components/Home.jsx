32c51635d8e44382851a191fc0421405a11d5c6d
pawan-30 <97761844+pawan-30@users.noreply.github.com>
2024-09-05 13:20:01 +0530

    Add files via upload

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,65 @@
+import React,  { useEffect } from 'react';
+
+import { Box, styled } from '@mui/material';
+
+import NavBar from './home/NavBar';
+import Banner from './home/Banner';
+import MidSlide from './home/MidSlide';
+import MidSection from './home/MidSection';
+import Slide from './home/Slide';
+
+import { useSelector, useDispatch } from 'react-redux'; // hooks
+import { getProducts as listProducts } from '../redux/actions/productActions';
+
+const Component = styled(Box)`
+    padding: 20px 10px;
+    background: #F2F2F2;
+`;
+
+const Home = () => {
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, error } = getProducts;
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch])
+
+    return (
+        <>
+            <NavBar />
+            <Component>
+                <Banner />
+                <MidSlide products={products} />
+                <MidSection />
+                <Slide
+                    data={products} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
+            </Component>
+        </>
+    )
+}
+
+export default Home;
---
+++ server/database/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+    //const URL = `mongodb://${username}:${password}@ecommerce-web-shard-00-00.wnaj9.mongodb.net:27017,ecommerce-web-shard-00-01.wnaj9.mongodb.net:27017,ecommerce-web-shard-00-02.wnaj9.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-sjmqa0-shard-0&authSource=admin&retryWrites=true&w=majority`
+    const URL = `mongodb+srv://${username}:${password}@e-commerce.63rqryd.mongodb.net/?retryWrites=true&w=majority&appName=E-Commerce`
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
+
+
+ 
